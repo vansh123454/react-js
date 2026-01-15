ce849f094803fd2244bd37b8fcd0a478f56aa76f
Vansh <vansh@LAPTOP-SKLJ3G38.com>
2026-01-15 16:40:48 +0530

    useState Hook, Counter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // it will work as well
+  // let [counter, setCounter] = useState(15); // setCounter is method which is responsible for counter's updation
+
+  const [counter, setCounter] = useState(15); // setCounter is method which is responsible for counter's updation
+
+  // let counter = 15;
+
+  // const increseValue = () =>{
+  //   // console.log("clicked", Math.random());
+  //   console.log("clicked", counter);
+  //   counter = counter + 1; // it will not reflect to the UI
+  // }
+  const increseValue = () =>{
+    if(counter === 20){}
+    else setCounter(counter + 1);
+  }
+
+  const decreseValue = () =>{
+    if(counter === 0) {}
+    else setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>Hey Vansh</h1>
+      <h2>counter value: {counter}</h2>
+      <button 
+      onClick={increseValue}
+      >Increse by 1 : {counter} </button>
+      <br />
+      <button
+      onClick={decreseValue}
+      >Decrese by 1 : {counter} </button>
+    </>
+  )
+}
+
+export default App
