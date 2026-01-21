fbdeb7cbb6346fc673b98ff0528d74914f2d2ba5
VanshBamborde <vanshbamborde882005@gmail.com>
2026-01-21 03:34:44 +0000

    reduxToolkit installation completed

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-red-500'>Redux toolkit</h1>
+    </>
+  )
+}
+
+export default App
